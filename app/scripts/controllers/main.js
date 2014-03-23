587c56cc942199d1972a4c99b396301f2b5ba89b
littlesorry <chaumet.g@gmail.com>
2014-03-23 20:28:26 +0800

    angular setup

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('drivercallApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
